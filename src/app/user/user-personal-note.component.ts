289591e38124b940ec88831d0b6fe7966f77ce95
Paweł Sobolewski <pawel.sobolewski@ds-consultants.eu>
2018-05-08 10:42:40 +0200

    Watermarks on page 1 and 2

--- src/app/user/user-personal-note.component.ts
+++ src/app/user/user-personal-note.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, style } from '@angular/core';
 
 @Component({
     selector: 'app-personal-note',
-    templateUrl: './user-personal-note.component.html'
+    templateUrl: './user-personal-note.component.html',
     styles: ['.personal-note-line { border-top: 3px solid #5791b9; padding: 25px 0 0 0; }']
 })
 export class UserPersonalNoteComponent {
