4f83c138bee6eb3db0ff56df28d82e546479a777
Paweł Sobolewski <pawel.sobolewski@ds-consultants.eu>
2018-05-21 14:33:26 +0200

    Inline editing - title

--- src/app/user/user-header.component.ts
+++ src/app/user/user-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -9,10 +10,27 @@ export class UserHeaderComponent {
     @Input() name: string;
     @Input() title: string;
     @Output() updateUser = new EventEmitter<any>();
+    titles = [
+        'Junior Front-end Developer',
+        'Senior Front-end Developer',
+        'Front-end Developer',
+        'CQ/AEM Expert',
+        'Junior AEM Developer',
+        'Senior AEM Developer',
+        'AEM Developer',
+        'QA Consultant'
+    ];
 
-    saveUser(e) {
-        console.log('Name changed: ' + e);
-        this.updateUser.emit({name: e});
+    titleOptions = this.titles.map(title => {
+        return {
+            'value': title,
+            'text': title
+        };
+    });
+    saveUser(key, newValue) {
+        const params = new Object;
+        params[key] = newValue;
+        this.updateUser.emit(params);
     }
 
     constructor() { }
