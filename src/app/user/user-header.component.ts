9d3433beda24fd5014685142bc1af8dd4dd130e7
Paweł Sobolewski <pawel.sobolewski@ds-consultants.eu>
2018-05-21 14:53:40 +0200

    Changing title now changes also image

--- src/app/user/user-header.component.ts
+++ src/app/user/user-header.component.ts
@@ -10,23 +10,35 @@ export class UserHeaderComponent {
     @Input() name: string;
     @Input() title: string;
     @Output() updateUser = new EventEmitter<any>();
+    imgUrlFront = "/assets/img/front-end.svg";
+    imgUrlTester = "/assets/img/test.svg";
+    imgUrlAem = "/assets/img/cq5.svg";
+    imgUrlAnalyst = "/assets/img/bussinessAnalyst.svg";
+    imgUrlJava = "/assets/img/java.svg";
+
     titles = [
-        'Junior Front-end Developer',
-        'Senior Front-end Developer',
-        'Front-end Developer',
-        'CQ/AEM Expert',
-        'Junior AEM Developer',
-        'Senior AEM Developer',
-        'AEM Developer',
-        'QA Consultant'
+        { name: 'Junior Front-end Developer', image: this.imgUrlFront },
+        { name: 'Senior Front-end Developer', image: this.imgUrlFront },
+        { name: 'Front-end Developer', image: this.imgUrlFront },
+        { name: 'CQ/AEM Expert', image: this.imgUrlAem },
+        { name: 'Junior AEM Developer', image: this.imgUrlAem },
+        { name: 'AEM Developer', image: this.imgUrlAem },
+        { name: 'Senior AEM Developer', image: this.imgUrlAem },
+        { name: 'QA Consultant', image: this.imgUrlAnalyst },
     ];
 
     titleOptions = this.titles.map(title => {
         return {
-            'value': title,
-            'text': title
+            'value': title.name,
+            'text': title.name
         };
     });
+
+    getImageIcon() {
+        const src = this.titles.find(title => title.name === this.title);
+        return src.image;
+    }
+
     saveUser(key, newValue) {
         const params = new Object;
         params[key] = newValue;
