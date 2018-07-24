c6bb52369652f256823fa23a058850d8b10836db
Rafał Maksymczuk <r.maksymczuk@gmail.com>
2018-07-24 15:40:40 +0200

    Trim text before changing whitep spaces to '-'

--- src/app/core/parametrize.pipe.ts
+++ src/app/core/parametrize.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'parametrize'})
 export class ParametrizePipe implements PipeTransform {
   transform(value: string): string {
-    return value.toLowerCase().replace(/ /g, '-').trim();
+    return value.toLowerCase().trim().replace(/ /g, '-');
   }
 }
